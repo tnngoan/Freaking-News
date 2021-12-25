c38e910e9b93c5224f4e53f061b4c69e69b5a0c6
tnngoan <ngoan.n.tr@gmail.com>
2021-12-25 23:56:49 +0700

    enable search news by keyword

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const typingTimeoutRef = useRef(null);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+  const KEY = process.env.API_KEY;
+
+  async function getNews(q) {
+    let cancel;
+    axios({
+      method: "GET",
+      url: `https://newsapi.org/v2/everything?q=${query}&apiKey=${KEY}`,
+      page,
+      q,
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        const news = res.data.articles;
+        console.log("news", news);
+        setPosts((prevPosts) => [...prevPosts, ...news]);
+        console.log("posts", posts);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+      });
+    return () => cancel();
+  }
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPage(1);
+    if (typingTimeoutRef.current) {
+      clearTimeout(typingTimeoutRef.current);
+    }
+    typingTimeoutRef.current = setTimeout(() => {
+      const searchTerm = e.target.value;
+      getNews(searchTerm);
+      console.log(searchTerm);
+    }, 500);
+  };
+
+  useEffect(() => {
+    getNews(query);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-section">
+        <input
+          placeholder="Search for a topic"
+          type="text"
+          value={query}
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="posts-section">
+        {posts.map((post) => {
+          return (
+            <div className="post" key={post}>
+              <h3>{post.title}</h3>
+              <div className="card-content">
+                <img alt={post.title} src={post.urlToImage} />
+                <hr className="divider" />
+                <p>{post.description}</p>
+              </div>
+              <div></div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
